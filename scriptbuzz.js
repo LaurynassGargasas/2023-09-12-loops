c6847f90232bd95ae142c7e10a55f45870cb334c
LaurynassGargasas <laurynas.gargasas@gmail.com>
2023-09-13 09:47:28 +0300

    optimized

--- scriptbuzz.js
+++ scriptbuzz.js
@@ -1,39 +1,74 @@
-function fizzbuzz(){
-    for(let i = 1; i <= 100; i++){
+// function fizzbuzz(){
+//     for(let i = 1; i <= 100; i++){
 
-        if(i % 3 === 0){
-            console.log('Fizz')
-        } else if(i % 5 === 0){
-            console.log('Buzz')
-        }else if(i % 7 === 0){
-            console.log('Biff')
-        } else if(i % 9 === 0){
-            console.log('Fuzz')
-        } else if(i % 9 === 0 && i % 3 === 0){
-            console.log('FizzFuzz')
-        } else if(i % 7 === 0 && i % 9 === 0){
-            console.log('BuzzFuzz')
-        } else if(i % 9 === 0 && i % 5 === 0){
-            console.log('BiffFuzz')
-        } else if(i % 9 === 0 && i % 3 === 0 && i % 5 === 0){
-            console.log('FizzBuzzFuzz')
-        } else if(i % 9 === 0 && i % 7 === 0 && i % 3 === 0){
-            console.log('FizzBiffFuzz')
-        } else if(i % 9 === 0 && i % 7 === 0 && i % 5 === 0){
-            console.log('BuzzBiffFuzz')
-        } else if(i % 7 === 0 && i % 3 === 0){
-            console.log('FizzBiff')
-        } else if(i % 7 === 0 && i % 5 === 0){
-            console.log('BuzzBiff')
-        } else if(i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
-            console.log('FizzBuzzBiff')
-        } else if(i % 3 === 0 && i % 5 === 0 && i % 7 === 0 && i % 9 === 0) {
-            console.log('FizzBuzzBiffFuzz')
-        } 
-        else if(i % 3 === 0 && i % 5 === 0) {
-            console.log('FizzBuzz')
-        } else {console.log(i)}
+//         if(i % 3 === 0){
+//             console.log('Fizz')
+//         } else if(i % 5 === 0){
+//             console.log('Buzz')
+//         }else if(i % 7 === 0){
+//             console.log('Biff')
+//         } else if(i % 9 === 0){
+//             console.log('Fuzz')
+//         } else if(i % 9 === 0 && i % 3 === 0){
+//             console.log('FizzFuzz')
+//         } else if(i % 7 === 0 && i % 9 === 0){
+//             console.log('BuzzFuzz')
+//         } else if(i % 9 === 0 && i % 5 === 0){
+//             console.log('BiffFuzz')
+//         } else if(i % 9 === 0 && i % 3 === 0 && i % 5 === 0){
+//             console.log('FizzBuzzFuzz')
+//         } else if(i % 9 === 0 && i % 7 === 0 && i % 3 === 0){
+//             console.log('FizzBiffFuzz')
+//         } else if(i % 9 === 0 && i % 7 === 0 && i % 5 === 0){
+//             console.log('BuzzBiffFuzz')
+//         } else if(i % 7 === 0 && i % 3 === 0){
+//             console.log('FizzBiff')
+//         } else if(i % 7 === 0 && i % 5 === 0){
+//             console.log('BuzzBiff')
+//         } else if(i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+//             console.log('FizzBuzzBiff')
+//         } else if(i % 3 === 0 && i % 5 === 0 && i % 7 === 0 && i % 9 === 0) {
+//             console.log('FizzBuzzBiffFuzz')
+//         } 
+//         else if(i % 3 === 0 && i % 5 === 0) {
+//             console.log('FizzBuzz')
+//         } else {console.log(i)}
+//     }
+// }
+
+
+
+function fizzbuzz() {
+    for (let i = 1; i <= 100; i++) {
+        let output = '';
+
+        if (i % 3 === 0) {
+            output += 'Fizz';
+        }
+
+        if (i % 5 === 0) {
+            output += 'Buzz';
+        }
+
+        if (i % 7 === 0) {
+            output += 'Biff';
+        }
+
+        if (i % 9 === 0) {
+            output += 'Fuzz';
+        }
+
+        if (i % 11 === 0){
+            output += 'Bizz'
+        }
+
+        if (output === '') {
+            output = i;
+        }
+
+        console.log(output);
     }
 }
+
 
 fizzbuzz()
